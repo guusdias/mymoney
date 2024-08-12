7533af74e85ecc8c4d7531a8dc35fff0640d410e
guusdias <gustavohd34@gmail.com>
2024-08-12 16:58:56 -0300

    fix: add options on chart

---
+++ app/ui/dashboard/activity-container.tsx
@@ -0,0 +1,3 @@
+export default function ActivityContainer() {
+  return <section></section>;
+}
--- app/ui/dashboard/chart.tsx
+++ app/ui/dashboard/chart.tsx
@@ -30,7 +30,34 @@ export default function AnalyticsChart() {
 
   return (
     <div className="w-full md:col-span-4">
-      <h2 className={`font-bold mb-4 text-xl md:text-2xl`}>Analytics</h2>
+      <div className="flex items-center justify-between">
+        <h2 className={`font-bold mb-4 text-xl md:text-2xl`}>Analytics</h2>
+        <div className="flex flex-row gap-10   mb-4">
+          <span className="font-bold flex items-center gap-2 text-sm">
+            <div className="bg-lighterPurple w-2 h-2 rounded-full"></div>Outcome
+          </span>
+          <span className="font-bold flex items-center gap-2 text-sm">
+            <div className="bg-bluePool w-2 h-2 rounded-full"></div> Income
+          </span>
+        </div>
+        <div className="relative inline-block w-28">
+          <select className="block appearance-none w-full bg-darkPurple border border-gray-500 text-gray-300 py-2 px-3 pr-8 rounded-xl leading-tight focus:outline-none focus:border-gray-400">
+            <option>2020</option>
+            <option>2021</option>
+            <option>2022</option>
+            <option>2023</option>
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-500">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M5.5 7l4.5 4.5L14.5 7H5.5z" />
+            </svg>
+          </div>
+        </div>
+      </div>
       <div className="rounded-xl bg-darkPurple p-4">
         <div className="flex overflow-x-auto">
           <div

35f5846e79bd7ac692b526c054be1ad8db57dab1
guusdias <gustavohd34@gmail.com>
2024-08-12 16:42:28 -0300

    style: credit card container

--- app/ui/dashboard/credit-container.tsx
+++ app/ui/dashboard/credit-container.tsx
@@ -3,11 +3,11 @@ import CardCarrousel from "@/app/ui/dashboard/card-carrousel";
 
 export default function CreditContainer() {
   return (
-    <div className="bg-darkPurple px-5 py-5 rounded-3xl flex flex-col justify-center">
-      <div className="px-5">
+    <div className="bg-darkPurple px-6 py-6 rounded-3xl flex flex-col justify-center">
+      <div className="">
         <h1 className="font-bold text-2xl mb-4">My Card</h1>
         <p className="text-sm font-thin">Card Balance</p>
-        <p className="mb-3">${"14,443.44"}</p>
+        <p className="mb-3 font-bold">${"14,443.44"}</p>
       </div>
       <div className="flex flex-col h-full w-full items-center">
         <CreditCard />
